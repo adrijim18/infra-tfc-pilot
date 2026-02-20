50135d8259abcfc3b5740b71770acf7be1a8f740
adrijim18 <adrijimdav@gmail.com>
2026-02-20 13:38:59 -0600

    Update main.tf
    Change VM size due to Azure capacity restriction

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-tfc-pilot"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_B2s"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
 
@@ -87,3 +87,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
